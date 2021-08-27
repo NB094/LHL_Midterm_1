038d3314baf5d4a8e84f00db5ef7938f476cd800
Katrin Pochtar <86984728+Katrin-Pochtar@users.noreply.github.com>
2021-08-26 19:45:25 -0700

    Visualisations are updated

--- Katrin.py
+++ Katrin.py
@@ -1,32 +1,27 @@
 import pandas as pd
 import numpy as np
+from sklearn import linear_model, datasets
 
-passengers_all_car = pd.read_csv('passengers_all_2019.csv')
-flights_data_car = pd.read_csv('flights_data_all_2019_Months.csv')
-fuel_car = pd.read_csv('fuel_comsumption_all_2019.csv')
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 
-passengers_all_car = passengers_all_car[['passengers', 'month', 'carrier']]
-passengers_all_car['carrier'] = passengers_all_car['carrier'].astype(str)
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
 
-# passengers_all_car.drop(['AS' 'B6' 'AA' 'DL' 'UA' 'NK' 'WN' 'HA' 'G4' 'F9'], columns=['carrier'], inplace=True)
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 
+from sklearn import metrics
+from sklearn.metrics import classification_report
 
+import warnings
+warnings.filterwarnings('ignore')
 
-# # group by each month average
-# passengers_all_car = passengers_all_car.groupby(['carrier', 'month']).agg(monthly_passengers = ('passengers', 'sum')).reset_index()
-#
-# # average monthly number of passengers that were carried by different air carriers
-# passengers_all_car = passengers_all_car.groupby(['carrier']).agg(av_monthly_passengers = ('monthly_passengers', 'mean')).reset_index()
-# passengers_all_car['av_monthly_passengers'] = passengers_all_car['av_monthly_passengers'].astype(int)
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+from xgboost import XGBClassifier
 
-print(passengers_all_car[passengers_all_car['carrier'].isin(['AS', 'B6', 'AA', 'DL', 'UA', 'NK', 'WN', 'HA', 'G4', 'F9'])])
+from sklearn.metrics import accuracy_score
 
-
-
-
-
-
-
-
-
-
+df_flights_final = pd.read_csv(df_flights_final.csv)
